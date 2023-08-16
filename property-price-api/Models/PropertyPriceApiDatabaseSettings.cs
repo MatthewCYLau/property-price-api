efe74c46c4b3ba873d922a0e9d4f1f585775f7de
matthew.lau <matthew.lau@credera.co.uk>
2023-08-16 22:04:32 +0100

    feat: add db settings class

---
+++ property-price-api/Models/PropertyPriceApiDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace property_price_api.Models
+{
+	public class PropertyPriceApiDatabaseSettings
+	{
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string BooksCollectionName { get; set; } = null!;
+    }
+}
+
--- property-price-api/Program.cs
+++ property-price-api/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using property_price_api.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -7,6 +9,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
+
+// Add services to the container.
+builder.Services.Configure<PropertyPriceApiDatabaseSettings>(
+    builder.Configuration.GetSection("PropertyPriceApiDatabase"));
 
 var app = builder.Build();
 
