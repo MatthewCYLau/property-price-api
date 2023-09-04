c0d44b3bbee1f5b4e0ff504b8250c613b68e42d9
matthew.lau <matthew.lau@credera.co.uk>
2023-09-04 09:20:23 +0100

    feat: add user type class

---
+++ property-price-api/Models/UserType.cs
@@ -0,0 +1,12 @@
+﻿namespace property_price_api.Models
+{
+	public static class UserType
+	{
+        public const string Renter = "Renter";
+        public const string FirstTimeBuyer = "FirstTimeBuyer";
+        public const string HomeOwner = "HomeOwner";
+        public const string Landlord = "Landlord";
+    }
+
+}
+
