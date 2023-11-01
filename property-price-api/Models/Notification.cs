14092f3a4d082520b02771da5a5b3fe60801f732
matthew.lau <matthew.lau@credera.co.uk>
2023-11-01 21:37:53 +0000

    feat: add notification model

---
+++ property-price-api/Models/Notification.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace property_price_api.Models
+{
+	public class Notification: BaseModel
+	{
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string ReadStatus { get; set; } = null!;
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string PriceSuggestionId { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ActorId { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string NotifierId { get; set; }
+
+        public string NotificationType { get; set; } = null!;
+    }
+}
+
---
+++ property-price-api/Models/NotificationTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace property_price_api.Models
+{
+	public static class NotificationTypes
+	{
+        private const string AboveAsking = "AboveAsking";
+        private const string BelowAsking = "BelowAsking";
+    }
+}
+
