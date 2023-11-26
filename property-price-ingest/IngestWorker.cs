bf19ed0fb03da6823367e664895bc5bbf60534b9
matthew.lau <matthew.lau@credera.co.uk>
2023-11-26 11:34:33 +0000

    spike: tcp listener on port 8080

--- property-price-ingest/IngestWorker.cs
+++ property-price-ingest/IngestWorker.cs
@@ -1,4 +1,6 @@
-﻿using property_price_ingest.Services;
+﻿using System.Net;
+using System.Net.Sockets;
+using property_price_ingest.Services;
 
 namespace property_price_ingest;
 
@@ -22,6 +24,9 @@ public sealed class IngestWorker : BackgroundService
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         _logger.LogInformation("{0} is running...", nameof(IngestWorker));
+        TcpListener listener = new TcpListener(IPAddress.Any, 8080);
+        listener.Start();
+        _logger.LogInformation("Listening on port {0}...", 8080);
         using var scope = _serviceScopeFactory.CreateScope();
         var processService = scope.ServiceProvider.GetService<IScopedProcessingService>();
         await processService.GetDataAsync(stoppingToken, 2);
