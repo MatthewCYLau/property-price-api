b4e40f16ce8d1d16d37de3258ebcc71a4fbdffc3
matthew.lau <matthew.lau@credera.co.uk>
2023-12-08 14:51:27 +0000

    feat: stats endpoint

---
+++ property-price-api/Controllers/StatisticsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using property_price_api.Helpers;
+using property_price_api.Models;
+
+namespace property_price_api.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatisticsController : ControllerBase
+    {
+
+        [Authorize]
+        [HttpGet("price-suggestions")]
+        public ActionResult<PriceSuggestionsStatisticsResponse> GetPriceSuggestionsStatistics()
+        {
+            
+            return Ok(new PriceSuggestionsStatisticsResponse(0, 0, 0));
+        }
+    }
+}
+
---
+++ property-price-api/Models/PriceSuggestionsStatisticsResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace property_price_api.Models
+{
+	public class PriceSuggestionsStatisticsResponse
+	{
+        public int AboveAsking { get; set; }
+        public int Asking { get; set; }
+        public int BelowAsking { get; set; }
+
+        public PriceSuggestionsStatisticsResponse(int aboveAsking, int asking, int belowAsking)
+        {
+            AboveAsking = aboveAsking;
+            Asking = asking;
+            BelowAsking = belowAsking;
+        }
+    }
+}
+
