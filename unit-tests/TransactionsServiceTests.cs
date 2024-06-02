1b93b621bcb0a984cf70d51b90f57ae11bb4155a
Matthew Lau <lau.cy.matthew@gmail.com>
2024-06-02 17:38:28 +0100

    feat: integration tests for cosmos service

---
+++ unit-tests/TransactionsServiceTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using property_price_cosmos_db.Models;
+using property_price_cosmos_db.Services;
+
+namespace unit_tests;
+
+[Ignore("Test to be ran locally")]
+[TestFixture]
+[Category("Integration")]
+public class TransactionsServiceTests
+{
+    private ServiceProvider _serviceProvider;
+    
+    [SetUp]
+    public void Setup()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging(builder => builder.AddConsole().AddDebug());
+        services.Configure<CosmosDbOptions>(options =>
+        {
+            options.DatabaseId = "TransactionsDb";
+            options.ContainerId = "TransasctionsContainer";
+        });
+        services.AddSingleton<CosmosClient>(_ => new CosmosClient(""));
+        services.AddSingleton<ITransactionService, TransactionService>();
+        _serviceProvider = services.BuildServiceProvider();
+    }
+    
+    [Test]
+    public async Task GetTransactions()
+    {
+        var transactionService = _serviceProvider.GetService<ITransactionService>();
+        var transactions = await transactionService.GetMultipleAsync("SELECT * FROM c");
+        Console.WriteLine("Retrieved {0} transactions from database.", transactions.Count());
+        Assert.That(transactions.Count() > 0);
+    }
+}
