0a193adf1ac809eb925510d6ab4706dc5c893fb5
Matthew Lau <lau.cy.matthew@gmail.com>
2024-06-26 10:41:07 +0100

    chore: add docs

--- property-price-api/Helpers/Calculations.cs
+++ property-price-api/Helpers/Calculations.cs
@@ -2,6 +2,12 @@ namespace property_price_api.Helpers;
 
 public static class Calculations
 {
+    /// <summary>
+    /// Returns mean suggested price
+    /// </summary>
+    /// <param name="percentages">Percentage</param>
+    /// <param name="askingPrice">Asking price</param>
+    /// <returns>Means suggested price</returns>
     public static int MeanSuggestedPrice(List<int> percentages, int askingPrice)
     {
         var meanSuggestedPrice = (int)percentages.Select(i => ((decimal)i / 100 + 1) * askingPrice).Sum() / percentages.Count;
