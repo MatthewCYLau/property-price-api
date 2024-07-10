66042d7d753fbec2755c1952d84347718075893c
Matthew Lau <lau.cy.matthew@gmail.com>
2024-07-10 07:10:25 +0100

    feat: read only fields

--- unit-tests/PropertiesControllerTests.cs
+++ unit-tests/PropertiesControllerTests.cs
@@ -11,10 +11,13 @@ namespace unit_tests;
 
 public class PropertiesControllerTests
 {
-    
+
     private ServiceProvider _serviceProvider;
     private IFixture _fixture;
-    
+    private readonly string ListingURL = "https://www.rightmove.co.uk/properties/141178922#/?channel=RES_BUY";
+    private readonly string Address = "Cardinal Close, Worcester Park";
+    private readonly int AskingPrice = 555_000;
+
     [SetUp]
     public void Setup()
     {
@@ -22,7 +25,7 @@ public class PropertiesControllerTests
         services.AddLogging();
         _serviceProvider = services.BuildServiceProvider();
     }
-    
+
     [Test]
     public async Task GetPropertiesShouldReturnExceptionInvalidDates()
     {
@@ -35,14 +38,14 @@ public class PropertiesControllerTests
         var res = await propertiesController.Get(startDate, endDate);
         Assert.IsInstanceOf<BadRequestObjectResult>(res.Result);
     }
-    
+
     [Test]
     public async Task GetPropertiesShould()
     {
         ILogger<PropertiesController> logger = _serviceProvider.GetRequiredService<ILogger<PropertiesController>>();
         _fixture = new Fixture();
         var mockPropertyService = new Mock<IPropertyService>();
-        
+
         // given
         var startDate = new DateTime(2024, 01, 30);
         var endDate = new DateTime(2024, 05, 30);
@@ -51,16 +54,16 @@ public class PropertiesControllerTests
         {
             new()
             {
-                ListingUrl = "https://www.rightmove.co.uk/properties/141178922#/?channel=RES_BUY",
-                Address = "Cardinal Close, Worcester Park",
-                AskingPrice = 555_000
+                ListingUrl = ListingURL,
+                Address = Address,
+                AskingPrice = AskingPrice
             }
         };
         var testPropertyDtos = testPropertyDtos2.Concat(testPropertyDtos1).ToList();
-        
+
         mockPropertyService.Setup(x => x.GetProperties(startDate, endDate)).Returns(Task.FromResult(testPropertyDtos));
         var propertiesController = new PropertiesController(logger, mockPropertyService.Object);
-        
+
         // when
         var propertiesResult = await propertiesController.Get(startDate, endDate);
         OkObjectResult? okResult = propertiesResult.Result as OkObjectResult;
@@ -69,29 +72,29 @@ public class PropertiesControllerTests
         Assert.IsNotNull(okResult);
         Assert.That(okResult.StatusCode, Is.EqualTo(200));
         Assert.That(okResult.Value, Is.EqualTo(testPropertyDtos));
-        
+
         List<PropertyDto> resultPropertyDtos = (List<PropertyDto>)okResult.Value;
         Assert.That(resultPropertyDtos.Count, Is.EqualTo(6));
         Assert.That(resultPropertyDtos[0].AskingPrice, Is.EqualTo(555_000));
     }
-    
+
     [Test]
     public async Task GetPropertyByIdShould()
     {
         ILogger<PropertiesController> logger = _serviceProvider.GetRequiredService<ILogger<PropertiesController>>();
         var mockPropertyService = new Mock<IPropertyService>();
-        
+
         // given
         var testPropertyDto = new PropertyDto
         {
-            ListingUrl = "https://www.rightmove.co.uk/properties/141178922#/?channel=RES_BUY",
-            Address = "Cardinal Close, Worcester Park",
-            AskingPrice = 555_000
+            ListingUrl = ListingURL,
+            Address = Address,
+            AskingPrice = AskingPrice
         };
-        
+
         mockPropertyService.Setup(x => x.GetPropertyById("1")).Returns(Task.FromResult(testPropertyDto));
         var propertiesController = new PropertiesController(logger, mockPropertyService.Object);
-        
+
         // when
         var propertyResult = await propertiesController.GetPropertyById("1");
         OkObjectResult? okResult = propertyResult.Result as OkObjectResult;
@@ -99,20 +102,20 @@ public class PropertiesControllerTests
         // Assert
         Assert.That(okResult.StatusCode, Is.EqualTo(200));
         Assert.That(okResult.Value, Is.EqualTo(testPropertyDto));
-        
+
         var resultPropertyDto = (PropertyDto)okResult.Value;
         Assert.That(resultPropertyDto.AskingPrice, Is.EqualTo(555_000));
     }
-    
+
     [Test]
     public async Task GetPropertyByInvalidIdShouldNotFound()
     {
         ILogger<PropertiesController> logger = _serviceProvider.GetRequiredService<ILogger<PropertiesController>>();
         var mockPropertyService = new Mock<IPropertyService>();
-        
-        mockPropertyService.Setup(x => x.GetPropertyById("foo")).Returns(Task.FromResult((PropertyDto) null));
+
+        mockPropertyService.Setup(x => x.GetPropertyById("foo")).Returns(Task.FromResult((PropertyDto)null));
         var propertiesController = new PropertiesController(logger, mockPropertyService.Object);
-        
+
         // when
         var propertyResult = await propertiesController.GetPropertyById("foo");
         NotFoundResult? notFoundResult = propertyResult.Result as NotFoundResult;
