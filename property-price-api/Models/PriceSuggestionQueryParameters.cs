b096da2174b691d25352f46805c05e9c725d589b
Matthew Lau <lau.cy.matthew@gmail.com>
2024-07-28 09:42:37 +0100

    fix: price suggestion paginations

--- property-price-api/Models/PriceSuggestionQueryParameters.cs
+++ property-price-api/Models/PriceSuggestionQueryParameters.cs
@@ -1,11 +1,11 @@
 ﻿namespace property_price_api.Models
 {
-	public class PriceSuggestionQueryParameters
-	{
+    public class PriceSuggestionQueryParameters
+    {
 
-		public string? PropertyId { get; set; }
-        public int? Page { get; set; }
-        public int? PageSize { get; set; }
+        public string? PropertyId { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
 
     }
 }
--- property-price-api/Services/PriceSuggestionService.cs
+++ property-price-api/Services/PriceSuggestionService.cs
@@ -7,14 +7,14 @@ namespace property_price_api.Services
 {
     public interface IPriceSuggestionService
     {
-        Task<PriceSuggestionResponse> GetPriceSuggestions(string? propertyId, int? page, int? pageSize);
+        Task<PriceSuggestionResponse> GetPriceSuggestions(string? propertyId, int page, int pageSize);
         Task<PriceSuggestion?> GetPriceSuggestionById(string id);
         Task CreatePriceSuggestion(PriceSuggestion priceSuggestion);
         Task DeletePriceSuggestionById(string id);
         Task<PriceSuggestionsStatisticsResponse> GetPriceSuggestionsStatistics();
     }
 
-    public class PriceSuggestionService: IPriceSuggestionService
+    public class PriceSuggestionService : IPriceSuggestionService
     {
         private readonly MongoDbContext _context;
         private readonly IPropertyService _propertyService;
@@ -37,7 +37,7 @@ namespace property_price_api.Services
 
         public async Task CreatePriceSuggestion(PriceSuggestion priceSuggestion)
         {
-            
+
             var httpContext = _httpContextAccessor.HttpContext;
             var userDto = (Task<UserDto>)httpContext!.Items["User"];
             var userId = userDto.Result.Id;
@@ -46,12 +46,12 @@ namespace property_price_api.Services
             {
                 throw new CustomException("Invalid property ID");
             }
-            
+
             if (GetPriceSuggestionByPropertyIdByUserId(priceSuggestion.PropertyId, userId).Result != null)
             {
                 throw new CustomException("User has already created price suggestion for property");
             }
-            
+
             priceSuggestion.Created = DateTime.Now;
             priceSuggestion.UserId = userId;
 
@@ -67,7 +67,7 @@ namespace property_price_api.Services
             var suggestion = await _context.PriceSuggestions.Find(x => x.Id == id).FirstOrDefaultAsync();
             return suggestion;
         }
-        
+
         private async Task<PriceSuggestion> GetPriceSuggestionByPropertyIdByUserId(string propertyId, string userId)
         {
             Expression<Func<PriceSuggestion, bool>> filter = x => x.PropertyId == propertyId && x.UserId == userId;
@@ -78,13 +78,13 @@ namespace property_price_api.Services
         public async Task DeletePriceSuggestionById(string id) =>
           await _context.PriceSuggestions.DeleteOneAsync(x => x.Id == id);
 
-        public async Task<PriceSuggestionResponse> GetPriceSuggestions(string? propertyId, int? page, int? pageSize = 5)
+        public async Task<PriceSuggestionResponse> GetPriceSuggestions(string? propertyId, int page, int pageSize)
         {
-            Expression<Func<PriceSuggestion,bool>> expression;
+            Expression<Func<PriceSuggestion, bool>> expression;
             if (propertyId != null)
             {
                 ValidatePropertyId(propertyId);
-                 expression = x => x.PropertyId == propertyId;
+                expression = x => x.PropertyId == propertyId;
             }
             else
             {
@@ -96,13 +96,13 @@ namespace property_price_api.Services
             var priceSuggestions = await _context.PriceSuggestions.Aggregate()
                 .Match(expression)
                 .Lookup(CollectionNames.PropertiesCollection, "PropertyId", "_id", @as: "Property")
-                .Skip((long)((page - 1) * pageSize))
-                .Limit((long)pageSize)
+                .Skip((page - 1) * pageSize)
+                .Limit(pageSize)
                 .Unwind("Property")
                 .As<PriceSuggestion>()
                 .ToListAsync();
 
-            return new PriceSuggestionResponse( new PaginationMetadata((int)totalRecordsCount, currentPage: (int)page, (int)Math.Ceiling(totalRecordsCount / (double)pageSize)), priceSuggestions);
+            return new PriceSuggestionResponse(new PaginationMetadata((int)totalRecordsCount, currentPage: page, (int)Math.Ceiling(totalRecordsCount / (double)pageSize)), priceSuggestions);
         }
 
 
@@ -121,9 +121,9 @@ namespace property_price_api.Services
             var belowAskingCount = await GetPriceSuggestionsStatisticsByExpression(x => x.DifferenceInPercentage < 0);
 
             return new PriceSuggestionsStatisticsResponse(
-                (int) aboveAskingCount,
-                (int) atAskingCount,
-                (int) belowAskingCount
+                (int)aboveAskingCount,
+                (int)atAskingCount,
+                (int)belowAskingCount
                 );
         }
 
