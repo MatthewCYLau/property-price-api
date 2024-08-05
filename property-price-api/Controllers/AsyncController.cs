86f3e99fd166514641e8ef924c4e101003cf2a09
Matthew Lau <lau.cy.matthew@gmail.com>
2024-08-05 07:14:32 +0100

    feat: async task when all endpoint

---
+++ property-price-api/Controllers/AsyncController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace property_price_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AsyncController : ControllerBase
+    {
+
+        private static async Task<int> GetRandomIntegerAsync()
+        {
+            await Task.Yield();
+            return new Random().Next(0, 1_000);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TaskWhenAll()
+        {
+
+            var randomOne = Task.Run(GetRandomIntegerAsync);
+            var randomTwo = Task.Run(GetRandomIntegerAsync);
+
+            await Task.WhenAll(randomOne, randomTwo);
+            var sum = randomOne.Result + randomTwo.Result;
+            return Ok(sum);
+        }
+    }
+}
+
+
