868371fba67e4299c5a48265079f6ec6be0e7a2a
Matthew Lau <lau.cy.matthew@gmail.com>
2024-09-04 21:34:51 +0100

    feat: scaffolding for nuget package

---
+++ property-price-common/CalculationUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace property_price_common;
+
+public static class CalculationUtils
+{
+    private static readonly Random random = new();
+
+    public static int GenerateRandomInteger(int
+ minValue, int maxValue)
+    {
+        return random.Next(minValue, maxValue + 1);
+    }
+}
