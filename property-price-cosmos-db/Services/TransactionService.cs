95021ed5ee028230652328a000626b1165eede90
Matthew Lau <lau.cy.matthew@gmail.com>
2024-10-11 17:32:51 +0100

    feat: use common package

--- property-price-cosmos-db/Services/TransactionService.cs
+++ property-price-cosmos-db/Services/TransactionService.cs
@@ -11,6 +11,7 @@ using property_price_cosmos_db.Models;
 using Azure.Messaging.ServiceBus;
 using Newtonsoft.Json;
 using System.Text;
+using property_price_common;
 
 namespace property_price_cosmos_db.Services;
 
@@ -342,7 +343,7 @@ patchOperations: [PatchOperation.Replace($"/comments", updatedComments)]);
         var transactions = await GetTransactionsByUserId(id);
         var blobServiceClient = _azureClientFactory.CreateClient("main");
         var blobContainerClient = blobServiceClient.GetBlobContainerClient(id);
-        var csvName = "";
+        var csvName = FileNameUtils.GenerateTransactionCsvFileName();
         BlobClient blobClient = blobContainerClient.GetBlobClient(csvName);
 
         using (var writer = new StringWriter())
