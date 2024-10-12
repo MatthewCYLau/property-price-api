175a690ecb73ffd1857ef700da883eaea155179a
Matthew Lau <lau.cy.matthew@gmail.com>
2024-10-12 20:43:14 +0100

    chore: remove unsed namespace

--- property-price-cosmos-db/Services/PaymentRequestService.cs
+++ property-price-cosmos-db/Services/PaymentRequestService.cs
@@ -1,17 +1,10 @@
-using System.Globalization;
 using Microsoft.Azure.Cosmos;
-using Azure.Storage.Blobs;
-using CsvHelper;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Options;
-using Azure.Storage.Blobs.Models;
-using Azure.Storage.Blobs.Specialized;
-using Azure.Storage.Sas;
 using property_price_cosmos_db.Models;
 using Azure.Messaging.ServiceBus;
 using Newtonsoft.Json;
 using System.Text;
-using property_price_common;
 
 namespace property_price_cosmos_db.Services;
 
@@ -19,11 +12,12 @@ public class PaymentRequestService : IPaymentRequestService
 {
     private readonly CosmosClient _client;
     private readonly CosmosDbOptions _options;
-    private Container _container;
+    private readonly Container _container;
     private readonly ILogger _logger;
     private readonly IAzureClientFactory<ServiceBusSender> _serviceBusSenderFactory;
 
     public PaymentRequestService(
+        ILogger<PaymentRequestService> logger,
         IOptions<CosmosDbOptions> options,
         CosmosClient client,
         IAzureClientFactory<ServiceBusSender> serviceBusSenderFactory
@@ -31,12 +25,14 @@ public class PaymentRequestService : IPaymentRequestService
     {
         _client = client;
         _options = options.Value;
+        _logger = logger;
         _serviceBusSenderFactory = serviceBusSenderFactory;
         _container = _client.GetContainer(_options.DatabaseId, _options.PaymentRequestsContainerId);
     }
 
     public async Task<Result> CreatePaymentRequest(PaymentRequest request)
     {
+        _logger.LogInformation("Creating payment request from {DebtorUserId} to {CreditorUserId}", request.DebtorUserId, request.CreditorUserId);
         await _container.CreateItemAsync(request, new PartitionKey(request.Id.ToString()));
         var _sender = _serviceBusSenderFactory.CreateClient("queue-sender");
         string messageBody = JsonConvert.SerializeObject(request);
