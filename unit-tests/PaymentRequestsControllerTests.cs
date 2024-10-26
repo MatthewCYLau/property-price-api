4ae5fbeedbf3085e313dfbe500023b993061ec8d
Matthew Lau <lau.cy.matthew@gmail.com>
2024-10-26 08:30:42 +0100

    feat: payment requests controller unit test #72

---
+++ unit-tests/PaymentRequestsControllerTests.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using property_price_cosmos_db.Controllers;
+using property_price_cosmos_db.Models;
+using property_price_cosmos_db.Services;
+
+namespace unit_tests;
+
+public class PaymentRequestsControllerTests
+{
+    [Test]
+    public async Task CreateTransactionIdShould()
+    {
+
+        var mockPaymentRequestService = new Mock<IPaymentRequestService>();
+        Mock<IConfiguration> mockConfiguration = new();
+        var debtorUserId = Guid.NewGuid();
+        var creditorUserId = Guid.NewGuid();
+        var request = new PaymentRequest { DebtorUserId = debtorUserId, CreditorUserId = creditorUserId, Amount = 10 };
+        mockPaymentRequestService.Setup(x => x.CreatePaymentRequest(request)).Returns(Task.FromResult(Result.Failure(PaymentRequestErrors.CreditorAndDebtorIdentical(creditorUserId.ToString(), debtorUserId.ToString()))));
+        var paymentRequestsController = new PaymentRequestsController(mockPaymentRequestService.Object, mockConfiguration.Object);
+        var createPaymentRequestResult = await paymentRequestsController.CreatePaymentRequest(request);
+        BadRequestObjectResult? result = createPaymentRequestResult as BadRequestObjectResult;
+
+        // Assert
+        Assert.That(result.Value, Is.Not.Null);
+        Assert.That(result.Value.ToString().Contains("PaymentRequestErrors.CreditorAndDebtorIdentical"), Is.True);
+        Assert.AreEqual(result.StatusCode, 400);
+    }
+}
