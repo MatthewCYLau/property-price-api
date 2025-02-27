d3976ed4cb5c65817d161c48cd6d5da4a55ab648
Matthew Lau <lau.cy.matthew@gmail.com>
2025-02-27 21:02:20 +0000

    feat: confluent kafka #81

--- property-price-kafka-consumer/Services/ConsumerService.cs
+++ property-price-kafka-consumer/Services/ConsumerService.cs
@@ -9,6 +9,7 @@ public class ConsumerService : BackgroundService
     private readonly IConsumer<Ignore, string> _consumer;
 
     private readonly ILogger<ConsumerService> _logger;
+    private readonly IConfiguration _configuration;
 
     public ConsumerService(IConfiguration configuration, ILogger<ConsumerService> logger)
     {
@@ -20,13 +21,13 @@ public class ConsumerService : BackgroundService
             GroupId = "InventoryConsumerGroup",
             AutoOffsetReset = AutoOffsetReset.Earliest
         };
-
+        _configuration = configuration;
         _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _consumer.Subscribe("InventoryUpdates");
+        _consumer.Subscribe(_configuration["Kafka:Topic"]);
 
         while (!stoppingToken.IsCancellationRequested)
         {
