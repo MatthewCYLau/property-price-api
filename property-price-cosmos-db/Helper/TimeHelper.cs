d6acb8bcac032ae08539e9d264b2479e14791170
Matthew Lau <lau.cy.matthew@gmail.com>
2025-05-08 22:19:17 +0100

    feat: validate time zone ID

--- property-price-cosmos-db/Helper/TimeHelper.cs
+++ property-price-cosmos-db/Helper/TimeHelper.cs
@@ -1,12 +1,36 @@
 namespace property_price_cosmos_db.Helper;
 
-public class TimeHelper
+public static class TimeHelper
 {
     public static DateTime GetCurrentTimeByTimeZoneId(string timeZoneId)
     {
-        DateTime utcNow = DateTime.UtcNow;
-        TimeZoneInfo targetTimeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
-        DateTime targetLocalTime = TimeZoneInfo.ConvertTimeFromUtc(utcNow, targetTimeZone);
-        return targetLocalTime;
+        if (IsValidTimeZoneId(timeZoneId))
+        {
+            DateTime utcNow = DateTime.UtcNow;
+            TimeZoneInfo targetTimeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+            DateTime targetLocalTime = TimeZoneInfo.ConvertTimeFromUtc(utcNow, targetTimeZone);
+            return targetLocalTime;
+        }
+        else
+        {
+            return DateTime.UtcNow;
+        }
+    }
+
+    public static bool IsValidTimeZoneId(string timeZoneId)
+    {
+        var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+        var logger = loggerFactory.CreateLogger(string.Empty);
+        foreach (TimeZoneInfo z in TimeZoneInfo.GetSystemTimeZones())
+        {
+            if (z.Id == timeZoneId)
+            {
+
+                logger.LogInformation("{0} is a valid time zone ID", timeZoneId);
+                return true;
+            }
+        }
+        logger.LogWarning("{0} is an invalid time zone ID!", timeZoneId);
+        return false;
     }
 }
