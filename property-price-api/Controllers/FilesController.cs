572ef9e0405757fca37beb4fa693cabf26763183
Matthew Lau <lau.cy.matthew@gmail.com>
2025-09-26 07:32:26 +0100

    feat: IFormFile open stream

--- property-price-api/Controllers/FilesController.cs
+++ property-price-api/Controllers/FilesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using property_price_api.Helpers;
+using property_price_api.Models;
 using property_price_api.Services;
 
 namespace property_price_api.Controllers
@@ -7,19 +8,29 @@ namespace property_price_api.Controllers
 
     [ApiController]
     [Route("api/[controller]")]
-    public class FilesController : ControllerBase
+    public class FilesController(
+        IGoogleCloudStorageService googleCloudStorageService,
+        ILogger<FilesController> logger,
+        IPropertyService propertyService) : ControllerBase
     {
-        private readonly IGoogleCloudStorageService _googleCloudStorageService;
-
-        public FilesController(IGoogleCloudStorageService googleCloudStorageService)
-        {
-            _googleCloudStorageService = googleCloudStorageService;
-        }
+        private readonly IGoogleCloudStorageService _googleCloudStorageService = googleCloudStorageService;
+        private readonly IPropertyService _propertyService = propertyService;
+        private readonly ILogger _logger = logger;
 
         [Authorize]
         [HttpPost]
         public async Task<IActionResult> UploadFile([FromForm] IFormFile file)
         {
+
+            using (var stream = file.OpenReadStream())
+            {
+                var createPropertyRequests = _propertyService.ReadCSV<CreatePropertyRequest>(stream);
+                foreach (var req in createPropertyRequests)
+                {
+                    _logger.LogInformation("Reading property for address {address}", req.Address);
+                }
+            }
+
             var url = await _googleCloudStorageService.UploadFileAsync(file);
             return Ok(new { file.FileName, url });
         }
